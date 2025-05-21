f0d0449076f5cfbdb5f0d24ba976a6f3297c6bee
galonso <galonso@intelix.biz>
2025-05-21 15:52:12 -0400

    mejoras

--- src/main/java/com/credix/edcHtmlToPdf/edcHtmlToPdfApplication.java
+++ src/main/java/com/credix/edcHtmlToPdf/edcHtmlToPdfApplication.java
@@ -12,7 +12,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableJSONDoc
 @EnableAsync
-@PropertySource("file:/home/produccion/conf/db.properties")
 @PropertySource("file:/home/produccion/conf/app.properties")
 
 public class edcHtmlToPdfApplication {
